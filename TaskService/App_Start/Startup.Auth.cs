71457f2347ecf3d5dc7764154d06ae333cde1117
dstrockis <dastrock@microsoft.com>
2016-07-20 13:33:32 -0700

    Minor bug fix part two

--- TaskService/App_Start/Startup.Auth.cs
+++ TaskService/App_Start/Startup.Auth.cs
@@ -36,13 +36,13 @@ namespace TaskService
             {
                 // This is where you specify that your API only accepts tokens from its own clients
                 ValidAudience = clientId,
+                AuthenticationType = policy,
             };
 
             return new OAuthBearerAuthenticationOptions
             {
                 // This SecurityTokenProvider fetches the Azure AD B2C metadata & signing keys from the OpenIDConnect metadata endpoint
                 AccessTokenFormat = new JwtFormat(tvps, new OpenIdConnectCachingSecurityTokenProvider(String.Format(aadInstance, tenant, policy))),
-                AuthenticationType = policy,
             };
         }
     }
