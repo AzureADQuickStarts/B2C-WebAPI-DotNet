f660debb7aec0be9d0cfa7fb7a7099fadd4ed48e
ChenYan98 <chenyan98@outlook.com>
2016-09-08 14:21:45 -0700

    Eliminate .Result and remove synclocks from OpenIdConnectCachingSecurityTokenProvider

--- TaskService/App_Start/OpenIdConnectCachingSecurityTokenProvider.cs
+++ TaskService/App_Start/OpenIdConnectCachingSecurityTokenProvider.cs
@@ -21,8 +21,6 @@ namespace TaskService.App_Start
         private IEnumerable<SecurityToken> _tokens;
         private readonly string _metadataEndpoint;
 
-        private readonly ReaderWriterLockSlim _synclock = new ReaderWriterLockSlim();
-
         public OpenIdConnectCachingSecurityTokenProvider(string metadataEndpoint)
         {
             _metadataEndpoint = metadataEndpoint;
@@ -41,16 +39,7 @@ namespace TaskService.App_Start
         {
             get
             {
-                RetrieveMetadata();
-                _synclock.EnterReadLock();
-                try
-                {
-                    return _issuer;
-                }
-                finally
-                {
-                    _synclock.ExitReadLock();
-                }
+                return _issuer;
             }
         }
 
@@ -64,32 +53,17 @@ namespace TaskService.App_Start
         {
             get
             {
-                RetrieveMetadata();
-                _synclock.EnterReadLock();
-                try
-                {
-                    return _tokens;
-                }
-                finally
-                {
-                    _synclock.ExitReadLock();
-                }
+                return _tokens;
             }
         }
 
-        private void RetrieveMetadata()
+        private async Task<OpenIdConnectConfiguration> RetrieveMetadata()
         {
-            _synclock.EnterWriteLock();
-            try
-            {
-                OpenIdConnectConfiguration config = Task.Run(_configManager.GetConfigurationAsync).Result;
-                _issuer = config.Issuer;
-                _tokens = config.SigningTokens;
-            }
-            finally
-            {
-                _synclock.ExitWriteLock();
-            }
+            OpenIdConnectConfiguration config = await _configManager.GetConfigurationAsync();
+            _issuer = config.Issuer;
+            _tokens = config.SigningTokens;
+
+            return config;
         }
     }
 }
